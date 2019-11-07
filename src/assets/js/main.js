f0c26d2a73400f0a6d4e601b9fad5f9eefcf3a50
硫酸 <lsch001@163.com>
2019-11-07 12:25:37 +0800

    :construction: Building

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -97,31 +97,86 @@ window.h5 = {
     }
 };
 window.onload = function() {
-    // window.h5.init();
-    var c = document.getElementById('container');
-    var box = document.getElementById('box');
+    /********************
+  TEST BRO
+  ********************/
 
-    window.addEventListener('devicemotion', function(e) {
-        console.log(e);
-        var ps = document.querySelectorAll('p');
-        ps[0] = 'a' + e.rotationRate.aplha;
-        ps[1] = 'b' + e.rotationRate.beta;
-        ps[2] = 'g' + e.rotationRate.gamma;
-        ps[3].textContent = e.accelerationIncludingGravity.x;
-        ps[4].textContent = e.accelerationIncludingGravity.y;
-        ps[5].textContent = e.accelerationIncludingGravity.z;
+    function handleMotionEvent(event) {
+        var x = event.accelerationIncludingGravity.x;
+        var y = event.accelerationIncludingGravity.y;
+        var z = event.accelerationIncludingGravity.z;
+        x = oneDecimal(x);
+        y = oneDecimal(y);
+        z = oneDecimal(z);
+
+        // Show motion info
+        $('.x_axis .value').text(x);
+        $('.y_axis .value').text(y);
+        $('.z_axis .value').text(z);
+
+
+        // Now animate the .thePlayer
+        var plyr = $('.such_ball_much_wow');
+
+        var winWidth = $(window).width();
+        var winHeight = $(window).height();
+
+        var _pos = {
+            'x': winWidth / (100 / toPercentage(x, 1)),
+            'y': winHeight / (100 / toPercentage(y, 1))
+        };
+
+        $(plyr).css({
+            'right': _pos.x,
+            'top': _pos.y
+        });
+
+
+    }
+
+    window.addEventListener("devicemotion", handleMotionEvent, true);
+
+
+    function oneDecimal(n) {
+        var number = n;
+        var rounded = Math.round(number * 10) / 10;
+        return rounded;
+    }
+
+    function toPercentage(x, n) {
+        var p = 0;
+        if (n) {
+            p = ((x + 10) / 20) * 100;
+        } else {
+            p = ((x + 10) / 20);
+        }
+        return oneDecimal(p);
+    }
+
+
+
+
+
+    // Animations with the wows!
+    var animations = ['flash'];
+    var animationName = '';
+    // Show the wows
+    $('.wowCircle').on('mouseenter', function() {
+        animationName = animations[randInt(0, animations.length - 1)];
+
+        $('.wow-overlay').show();
+        $('.wow-overlay .wow').addClass('animated ' + animationName);
     });
-    /*
-    c.animate([
-      {transform: 'rotateY(-40deg) rotateZ(60deg)'},
-      {transform: 'rotateY(40deg) rotateZ(-60deg)'}
-    ], {
-      duration: 1600,
-      easing: 'ease-in-out',
-      direction: 'alternate',
-      iterations: Infinity
+
+    $('.wowCircle').on('mouseleave', function() {
+        $('.wow-overlay').hide();
+        $('.wow-overlay .wow').removeClass('animated ' + animationName);
     });
-    //*/
+
+    function randInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
 
 };
 
